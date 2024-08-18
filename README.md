4d33d6567ec633db1a1720c3d644fb7e6f40a720
matthew <matthewyt765@gmail.com>
2024-08-18 11:56:21 +0800

    First Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# My learning about C++
---
+++ helloWorld.cxx
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+
+    //This is a comment
+    /*
+    This
+    is 
+    a 
+    multi-line comment
+    */
+    std::cout << "Hello World!" << '\n';
+    std::cout << "I'm Matthew" << std::endl;
+    return 0;
+}
