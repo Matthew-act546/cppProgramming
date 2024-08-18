3bac46ac749c451cd9321f182753715aeaac3212
matthew <matthewyt765@gmail.com>
2024-08-18 13:17:32 +0800

    adding the using and typedef alliases files

---
+++ usingTypedefAliases.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+
+using varString = std::string;
+using integer = int;
+
+// using print = std::cout;
+// typedef std::string varString;
+int main() {
+    varString firstName = "Matthew";
+    integer age = 18;
+    std::cout << firstName << std::endl; 
+    std::cout << age << std::endl; 
+    return 0;
+}
