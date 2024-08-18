9db1f1f7fd73efb80ffae0b262e4264dc7c9b26e
matthew <matthewyt765@gmail.com>
2024-08-18 13:59:15 +0800

    adding the parenthessis arithmetic file

---
+++ parenthesisArithmetic.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    // MULTIPLICATION AND DIVISION
+    // ADDITION AND SUBTRACTION
+    // LEFT TO RIGHT ORDER WITH SOME PRIORITIES IN THE ABOVE
+
+    int operation = 6 - (5 + ((4 * 3) / 2));
+    std::cout << operation << std::endl;
+    return 0;
+}
